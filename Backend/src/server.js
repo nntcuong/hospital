4a172382fd32e8d8d2576aaf598b3daaa124a96f
nntcuong <thecuongyt3htk@gmail.com>
2024-04-05 21:06:02 +0700

    fdfd

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,14 +3,21 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from './routes/web';
 require('dotenv').config();
+import cors from 'cors';
 import connectDB from "./config/connectDB";
 let app = express();
 
-//config app
+//config appnpm start
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
-
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions));
 viewEngine(app);
 initWebRoutes(app);
 
